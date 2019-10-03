acb5a24714fa1e682038555d7187d56c15c2d4d4
dinesh181 <edineshkumar455@gmail.com>
2019-10-03 19:14:59 +0530

    first commit

---
+++ src/com/cognizant/cms/servlet/MemberRegistration.java
@@ -0,0 +1,94 @@
+package com.cognizant.cms.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.cognizant.cms.dao.AdminDaoSql;
+import com.cognizant.cms.dao.ConnectionHandler;
+import com.cognizant.cms.dao.MemberDaoSql;
+import com.cognizant.cms.model.Admin;
+import com.cognizant.cms.model.Member;
+
+@WebServlet("/MemberRegistration")
+public class MemberRegistration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public MemberRegistration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  // doGet(request, response);
+		   String fname = request.getParameter("fname");
+	       String lname=request.getParameter("lname");
+	       String agee=request.getParameter("age");
+	       int age=Integer.parseInt(agee);
+	       String gender=request.getParameter("gender");
+	       String contactnumberr=request.getParameter("phonenumber");
+	       String memberid=request.getParameter("member");
+	       String password=request.getParameter("password");
+	       String email=request.getParameter("email");
+	       String address=request.getParameter("address");
+	       String plan_code=request.getParameter("plan_code");
+	       String state=request.getParameter("state");
+	       String city=request.getParameter("city");
+	       String zip_code=request.getParameter("zip_code");
+	
+           MemberDaoSql memberDaoSql=new MemberDaoSql();
+           Member member=new Member();
+           member.setAddress(address);
+           member.setAge(age);
+           member.setCity(city);
+           member.setContactnumber(contactnumberr);
+           member.setEmail(email);
+           member.setFname(fname);
+           member.setLname(lname);
+           member.setGender(gender);
+           member.setMemberid(memberid);
+           member.setPassword(password);
+           member.setPlan_code(plan_code);
+           member.setState(state);
+           member.setZipcod(zip_code);
+           int i= memberDaoSql.verifysignup(member);
+           if(i==1){
+  			 
+    	     memberDaoSql.addMember(member);
+  			 String succes= "you have to wait for approval "+member.getFname();
+  			 request.setAttribute("registered", succes);
+  			 request.getRequestDispatcher("memberlogin.jsp").forward(request, response);
+  			 
+  		 }
+           if(i==2)
+    		 {
+    			 String str= "Your Request is already send for approval";
+    			 request.setAttribute("errormsg1", str);
+    			 request.getRequestDispatcher("memberlogin.jsp").forward(request, response);
+    		 }
+  		  if(i==3)
+  		 {
+  			 String str= "Your Request has been approvrd You can Login";
+  			 request.setAttribute("errormsg1", str);
+  			 request.getRequestDispatcher("memberlogin.jsp").include(request, response);
+  		 }
+	       
+	       
+	}
+
+}
